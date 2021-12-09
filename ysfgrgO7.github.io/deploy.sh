53b679a15242868e29da6c46b85d50febf407be1
ysfgrg <ysfgrg07@gmail.com>
2021-12-09 12:00:53 +0200

    nvoid

--- ysfgrgO7.github.io/deploy.sh
+++ ysfgrgO7.github.io/deploy.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env sh
 
 set -e
-
 npm run docs:build
-
 cd docs/.vuepress/dist
-
+echo 'www.nvoid.org' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
-
 git push -f git@github.com:ysfgrgO7/ysfgrgO7.github.io.git master
-
-
 cd -
