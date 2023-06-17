1ba7ef402318a3e35fe3c1e8292b9198144b1942
ysfgrgO7 <ysfgrgO7@gmail.com>
2023-06-17 16:24:07 +0300

    website

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ git init
 git branch -M Deployment
 git add -A 
 git commit -m 'deploy'
-git push -f git@github.com:nvoid-lua/nvoid.org.src Deployment
+git push -f git@github.com:nvoid-lua/nvoid.org Deployment
 cd -
 git add -A
 git commit -m 'website'
-git push -f git@github.com:nvoid-lua/nvoid.org.src main
+git push -f git@github.com:nvoid-lua/nvoid.org main
