abb6c01a3932b29e0e8dc78355db180be60ebcef
Youssef George <ysfgrg2007@gmail.com>
2024-06-04 15:29:32 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 set -e
+cd ./docs/
+rm -rf configuration
+git clone https://github.com/nvoid-lua/nvoidocs.git configuration
+cd ../
 yarn clear
 yarn build
 cd ./build/
 git init
 git branch -M Deployment
-git add -A 
+git add -A
 git commit -m 'deploy'
 git push -f git@github.com:nvoid-lua/nvoid.org Deployment
 cd -
