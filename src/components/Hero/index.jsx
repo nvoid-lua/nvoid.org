e367808d4a4d0034eef47ca91ad355201ea2b9ae
Youssef George <ysfgrg07@gmail.com>
2024-06-20 19:10:09 +0300

    website

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,52 +1,50 @@
-import React, { } from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import styles from './index.module.css';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import classnames from "classnames"
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import styles from "./index.module.css";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import classnames from "classnames";
+import PickVersion from "./pick";
 
 const Hero = () => {
-
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className={`container ${styles.container}`}>
         <div className={styles.heroContent}>
           <h1 className={`hero__title ${styles.heroTitle}`}>Nvoid</h1>
           <p className={`hero__description ${styles.heroDescription} `}>
-            An enhanced version of the {' '}
-            <a href='https://neovim.io' target={'_blank'}>
+            An enhanced version of the{" "}
+            <a href="https://neovim.io" target={"_blank"}>
               Neovim
-            </a>{' '}
-            {' '}text editor
+            </a>{" "}
+            text editor
           </p>
           <div className={styles.buttons}>
             <Link
-              className={classnames(
-                "button button--primary button--lg rounded-pill",
-                styles.button
-              )}
+              className={classnames("btn1 button--lg", styles.btn1)}
               to={useBaseUrl("/docs/install")}
             >
               Get Started
             </Link>
             <Link
-              className={classnames(
-                "button button--outline button--secondary button--lg rounded-pill",
-                styles.button
-              )}
+              className={classnames("btn2 button--lg", styles.btn2)}
               to={useBaseUrl("/docs/configuration")}
-            >
-              Config
-            </Link>
+            ></Link>
           </div>
-
-
         </div>
         <div className={styles.heroImage}>
           <img
-            src={require('@site/static/img/nvoid_logo.png').default}
+            src={require("@site/static/img/nvoid_logo.png").default}
             alt={"Nvoid logo"}
           />
+          {/* <h2 className="tw-inline-block tw-text-base lg:tw-text-lg tw-font-semibold tw-rounded-2xl tw-backdrop-blur-sm lg:tw-backdrop-blur"> */}
+          {/*   Install with one command */}
+          {/* </h2> */}
+          {/* <PickVersion className="!tw-mb-0"> */}
+          {/*   { */}
+          {/*     "bash <(curl -s https://raw.githubusercontent.com/nvoid-lua/nvoid/main/utils/installer/install.sh)" */}
+          {/*   } */}
+          {/* </PickVersion> */}
         </div>
       </div>
     </header>
