9f1176c972f5be7fa63a09d3d076f64deb5fa7b1
Youssef George <ysfgrg07@gmail.com>
2024-07-09 22:17:03 +0300

    website

--- src/theme/Navbar/index.js
+++ src/theme/Navbar/index.js
@@ -1,10 +1,82 @@
-import React from 'react';
-import NavbarLayout from '@theme/Navbar/Layout';
-import NavbarContent from '@theme/Navbar/Content';
+import React from "react";
+import NavbarLayout from "@theme/Navbar/Layout";
+import { useThemeConfig, ErrorCauseBoundary } from "@docusaurus/theme-common";
+import {
+  splitNavbarItems,
+  useNavbarMobileSidebar,
+} from "@docusaurus/theme-common/internal";
+import NavbarItem from "@theme/NavbarItem";
+import NavbarColorModeToggle from "@theme/Navbar/ColorModeToggle";
+import SearchBar from "@theme/SearchBar";
+import NavbarMobileSidebarToggle from "@theme/Navbar/MobileSidebar/Toggle";
+import NavbarSearch from "@theme/Navbar/Search";
+import NavbarLogo from "../../../static/img/icons/nvoid.svg";
+import Link from "@docusaurus/Link";
+import styles from "./styles.module.css";
+function useNavbarItems() {
+  // TODO temporary casting until ThemeConfig type is improved
+  return useThemeConfig().navbar.items;
+}
+function NavbarItems({ items }) {
+  return (
+    <>
+      {items.map((item, i) => (
+        <ErrorCauseBoundary
+          key={i}
+          onError={(error) =>
+            new Error(
+              `A theme navbar item failed to render.
+Please double-check the following navbar item (themeConfig.navbar.items) of your Docusaurus config:
+${JSON.stringify(item, null, 2)}`,
+              { cause: error },
+            )
+          }
+        >
+          <NavbarItem {...item} />
+        </ErrorCauseBoundary>
+      ))}
+    </>
+  );
+}
+function NavbarContentLayout({ left, right }) {
+  return (
+    <div className="navbar__inner">
+      <div className="navbar__items">{left}</div>
+      <div className="navbar__items navbar__items--right">{right}</div>
+    </div>
+  );
+}
+
 export default function Navbar() {
+  const mobileSidebar = useNavbarMobileSidebar();
+  const items = useNavbarItems();
+  const [leftItems, rightItems] = splitNavbarItems(items);
+  const searchBarItem = items.find((item) => item.type === "search");
   return (
     <NavbarLayout>
-      <NavbarContent />
+      <NavbarContentLayout
+        left={
+          <>
+            {!mobileSidebar.disabled && <NavbarMobileSidebarToggle />}
+            <Link to="/" className={styles.navbarlogo}>
+              <NavbarLogo className={styles.navbarsvg} />
+              <a className={styles.navbartext}>Nvoid</a>
+            </Link>
+            <NavbarItems items={leftItems} />
+          </>
+        }
+        right={
+          <>
+            <NavbarItems items={rightItems} />
+            <NavbarColorModeToggle className={styles.colorModeToggle} />
+            {!searchBarItem && (
+              <NavbarSearch>
+                <SearchBar />
+              </NavbarSearch>
+            )}
+          </>
+        }
+      />
     </NavbarLayout>
   );
 }
